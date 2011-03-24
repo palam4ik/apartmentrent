e33dca7e509c29b290b2792381ddd7461554e0bf
palam4ik <palam4ik@gmail.com>
2011-03-24 21:13:49 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apartments::Application.config.secret_token = '1e7189620ea0e53a78f29f18750ac8f42e61dec1943c4a5212c56394527ec303429cfae6ef65da97891ee4e7787861212af4341a92a9863788d71f0f28e44881'
